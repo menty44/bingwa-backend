8d46ce0290a4ee520e9185826cca272c7c27843f
Fredrick Oluoch <menty44@gmail.com>
2022-09-11 20:33:01 +0300

    Users blueprint

---
+++ src/datasources/bingwa.datasource.ts
@@ -0,0 +1,32 @@
+import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
+import {juggler} from '@loopback/repository';
+
+const config = {
+  name: 'bingwa',
+  connector: 'mongodb',
+  url: 'mongodb+srv://bingwa-root:bingwa1234@cluster0.vci6c.mongodb.net/test',
+  host: 'cluster0.vci6c.mongodb.net',
+  port: 27017,
+  user: 'bingwa-root',
+  password: 'bingwa1234',
+  database: 'bingwa',
+  useNewUrlParser: true
+};
+
+// Observe application's life cycle to disconnect the datasource when
+// application is stopped. This allows the application to be shut down
+// gracefully. The `stop()` method is inherited from `juggler.DataSource`.
+// Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
+@lifeCycleObserver('datasource')
+export class BingwaDataSource extends juggler.DataSource
+  implements LifeCycleObserver {
+  static dataSourceName = 'bingwa';
+  static readonly defaultConfig = config;
+
+  constructor(
+    @inject('datasources.config.bingwa', {optional: true})
+    dsConfig: object = config,
+  ) {
+    super(dsConfig);
+  }
+}
---
+++ src/models/users.model.ts
@@ -0,0 +1,40 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Users extends Entity {
+  @property({
+    type: 'string',
+    required: true,
+  })
+  first_name: string;
+
+  @property({
+    type: 'string',
+  })
+  last_name?: string;
+
+  @property({
+    type: 'string',
+    id: true,
+    generated: false,
+    required: true,
+  })
+  email: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  password: string;
+
+
+  constructor(data?: Partial<Users>) {
+    super(data);
+  }
+}
+
+export interface UsersRelations {
+  // describe navigational properties here
+}
+
+export type UsersWithRelations = Users & UsersRelations;
